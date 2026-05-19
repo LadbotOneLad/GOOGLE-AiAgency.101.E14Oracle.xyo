d37adb78c6a3830faab3eefe7e24ee0069436ff6
E14 Docker <e14@example.com>
2026-05-20 06:20:36 +1000

    Added mermaid-js, sodium, yt-dlp and project files

---
+++ google-cloud-sdk/cloud_env.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+CLS
+SET PATH=C:\~E14-\google-cloud-sdk\google-cloud-sdk\bin;%PATH%;
+cd C:\~E14-\google-cloud-sdk
+ECHO Welcome to the Google Cloud CLI! Run "gcloud -h" to get the list of available commands.
+ECHO ---
+ECHO ON
